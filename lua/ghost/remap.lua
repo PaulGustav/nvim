c098264350844dc7e602a6daf35a2c480354daee
janis <diabolo2110@gmail.com>
2025-03-08 23:54:29 +0100

    lazygit funktion als eigener buffer

--- lua/ghost/remap.lua
+++ lua/ghost/remap.lua
@@ -35,5 +35,31 @@ function OpenFloatingTerminal()
   vim.api.nvim_command("startinsert")
 end
 
+function OpenFloatingLazygit()
+  local buf = vim.api.nvim_create_buf(false, true)
+
+  local width = math.floor(vim.o.columns * 0.8)
+  local height = math.floor(vim.o.lines * 0.8)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = "minimal",
+    border = "rounded",
+  })
+
+  vim.fn.termopen("lazygit") -- Hier wird lazygit statt der Shell gestartet
+
+  vim.api.nvim_win_set_option(win, "number", false)
+  vim.api.nvim_win_set_option(win, "relativenumber", false)
+
+  vim.api.nvim_command("startinsert")
+end
 
 vim.keymap.set("n", "<leader>t", ":lua OpenFloatingTerminal()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>g", ":lua OpenFloatingLazygit()<CR>", { noremap = true, silent = true })
