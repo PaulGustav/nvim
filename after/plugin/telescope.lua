ef16013a497da1033382feb724913b444c283fdf
janis <diabolo2110@gmail.com>
2025-03-13 14:14:29 +0100

    telescope remap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>sg', builtin.git_files, {})
+vim.keymap.set('n', '<leader><leader>', builtin.buffers, {})
+-- viv.keymap.set('n', '<leader>sh', builtin., {}) Home Directory Searc
