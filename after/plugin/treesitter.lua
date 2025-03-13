1c847199eb6499b3f3074e246b88bc1e07101004
janis <diabolo2110@gmail.com>
2025-03-13 15:33:58 +0100

    update treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,7 +2,28 @@ vim.api.nvim_set_hl(0, "@keyword", { fg = "#b48ead" }) -- Nord 15, purple
 vim.api.nvim_set_hl(0, "@keyword.function", { fg = "#b48ead" }) -- Nord 15, purple
 
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "javascript", "python", "toml", "json", "sql", "typescript", "rust", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
+
+    ensure_installed = {
+        "javascript",
+        "python",
+        "cmake",
+        "css",
+        "svelte",
+        "fish",
+        "toml",
+        "json",
+        "sql",
+        "typescript",
+        "rust",
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "markdown",
+        "markdown_inline"
+    },
 
   sync_install = false,
 
