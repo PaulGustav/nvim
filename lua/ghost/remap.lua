1a50f859f0ad6eda08bec37db772b1014fc61c0b
janis <diabolo2110@gmail.com>
2025-03-13 15:21:21 +0100

    more remaps

--- lua/ghost/remap.lua
+++ lua/ghost/remap.lua
@@ -4,25 +4,31 @@ vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 -- Filesystem
 -- vim.keymap.set("n", "<leader>cd", vim.cmd.)
 
+-- Increment / Decrement
+vim.keymap.set("n", "+", "<C-a>")
+vim.keymap.set("n", "-", "<C-x>")
+
+local opts = { noremap = true, silent = true}
+
 -- Movement
-vim.api.nvim_set_keymap('n', '<leader>h', ':wincmd h<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>j', ':wincmd j<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>k', ':wincmd k<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>l', ':wincmd l<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>h', ':wincmd h<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>j', ':wincmd j<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>k', ':wincmd k<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>l', ':wincmd l<CR>', opts)
 
 -- Windows & Buffer
-vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<C-s>", vim.cmd.w)
 vim.keymap.set("n", "<leader>so", vim.cmd.w)
 vim.keymap.set("n", "<leader>v", vim.cmd.vnew)
 vim.keymap.set("n", "<leader>n", vim.cmd.new)
 vim.keymap.set("n", "<leader>o", ":Neotree filesystem reveal right<CR>")
 
-vim.keymap.set("n", "<leader>q", vim.cmd.q)
-vim.keymap.set("n", "<C-c>", ":bdelete!<CR>")
+vim.keymap.set("n", "<C-q>", vim.cmd.q)
+vim.keymap.set("n", "<leader>q", ":bdelete!<CR>")
 vim.keymap.set("t", "<esc>", "<C-\\><C-n>", { buffer = buf, noremap = true, silent = true })
 
-vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", opts)
+vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", opts)
 
 function OpenFloatingTerminal()
   local buf = vim.api.nvim_create_buf(false, true) 
@@ -79,5 +85,5 @@ function OpenFloatingLazygit()
   vim.api.nvim_command("startinsert")
 end
 
-vim.keymap.set("n", "<leader>t", ":lua OpenFloatingTerminal()<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>g", ":lua OpenFloatingLazygit()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>t", ":lua OpenFloatingTerminal()<CR>", opts)
+vim.keymap.set("n", "<leader>g", ":lua OpenFloatingLazygit()<CR>", opts)
