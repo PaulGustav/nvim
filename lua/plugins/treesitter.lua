bddfa8746abd474c519a2d8f8ff45aed0af784d5
janis <diabolo2110@gmail.com>
2025-03-14 20:20:59 +0100

    treesitter update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -32,4 +32,7 @@ return {
             additional_vim_regex_highlighting = false,
         },
     },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
 }
