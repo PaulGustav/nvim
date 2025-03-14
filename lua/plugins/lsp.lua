cc4f45fbe50d01fa1789e96153f5ad97fe26e1d1
janis <diabolo2110@gmail.com>
2025-03-14 20:49:15 +0100

    lazydev lsp added

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -54,4 +54,14 @@ return {
             })
         end,
     },
+    {
+        'folke/lazydev.nvim',
+        ft = 'lua',
+        opts = {
+            library = {
+                -- Load luvit types when the `vim.uv` word is found
+                { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+            },
+        },
+    },
 }
